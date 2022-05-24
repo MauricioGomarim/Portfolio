dc798849f70317691147cce433efdae285ebb4ee
Mauricio Gomarim <89900658+MauricioGomarim@users.noreply.github.com>
2022-05-24 16:55:29 -0700

    First Commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate'
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + 500
+  target.forEach(element => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass)
+    }
+  })
+}
+
+window.addEventListener('scroll', function () {
+  animeScroll()
+})
